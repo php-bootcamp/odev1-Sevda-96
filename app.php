feff886632ed9e3947c259fe925a19131555bc6d
Sevda Engin <53494300+Sevda-96@users.noreply.github.com>
2021-02-03 14:54:05 +0300

    Update app.php

--- app.php
+++ app.php
@@ -1,6 +1,11 @@
-<?php
+<form action="odev1.php" method="post">
+    <label>Yas:</label>
+    <input type="text" name="yas" />
+    <input type="submit" name="submit" />
+</form>
 
-$age = 25;
+<?php
+//$age = 25;
 
 /**
  * Bu ödevde age değişkeni içerisinde yer alan yaş değerinin bulunduğu
@@ -18,3 +23,35 @@ $age = 25;
  * 
  * İyi çalışmalar.
  */
+if(isset($_POST['submit'])){
+if(!empty($_POST['yas'])){
+    $yas=$_POST['yas'];
+    //var_dump($yas); 
+    $age=(int)$yas;
+    
+    switch($age){
+        case($age>=0 && $age<=14):
+          echo"<h1>Çocuk</h1>"."\n";
+          break;
+          case($age<=24 && $age>=15):
+              echo"<h1>Genç</h1>"."\n";
+              break;
+              case($age<=64 && $age>=25):
+                  echo"<h1>Yetişkin</h1>"."\n";
+                  break;
+                  case($age>=65):
+                      echo"<h1>Yaşlı</h1>"."\n";
+                      break;
+                      default:
+                      break;
+  
+    }
+}
+else{
+    echo"hata boş bırakılamaz!";
+}
+}
+
+ 
+
+?>
